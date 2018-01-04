0df7eb25cfa7d39486f37e469eaf5adffbe41880
yaya <denis_tsn@mail.ru>
2018-01-05 00:45:39 +0300

    v0.2

--- headlines.py
+++ headlines.py
@@ -1,11 +1,26 @@
+import feedparser
 from flask import Flask
 
+
 app = Flask(__name__)
+
+MAIL_RU_FEED = "https://news.mail.ru/rss"
 
 
 @app.route("/")
 def get_news():
-    return "no news is good news"
+    feed = feedparser.parse(MAIL_RU_FEED)
+    first_article = feed['entries'][0]
+    return """<html>
+        <body>
+            <h1> Новости Mail.ru </h1>
+                <b>{0}</b> <br/>
+                <i>{1}</i> <br/>
+                <p>{2}</p> <br/>
+        </body>
+    </html>""".format(first_article.get("title"),
+                      first_article.get("published"),
+                      first_article.get("summary"))
 
 
 if __name__ == "__main__":
